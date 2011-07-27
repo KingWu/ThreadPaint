529cc03beb68f4bfe695cb96db1036cf3b7003a4
Maximilian Fellner <max.fellner@gmail.com>
2011-07-27 14:14:02 +0200

    Improved horizontal color picker for low resolutions.

--- src/at/droidcode/threadpaint/ThreadPaintApp.java
+++ src/at/droidcode/threadpaint/ThreadPaintApp.java
@@ -1,15 +1,25 @@
 package at.droidcode.threadpaint;
 
 import android.app.Application;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class ThreadPaintApp extends Application {
-	private static final int MAX_STROKE_WIDTH_DP = 150;
+	private static final int MAX_STROKE_WIDTH_DP_VERT = 150;
+	private static final int MAX_STROKE_WIDTH_DP_HORZ = 125;
+
 	private int maxStrokeWidthPx;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		maxStrokeWidthPx = Utils.dp2px(getApplicationContext(), MAX_STROKE_WIDTH_DP);
+
+		final Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+		if (display.getOrientation() == 0) {
+			maxStrokeWidthPx = Utils.dp2px(getApplicationContext(), MAX_STROKE_WIDTH_DP_VERT);
+		} else {
+			maxStrokeWidthPx = Utils.dp2px(getApplicationContext(), MAX_STROKE_WIDTH_DP_HORZ);
+		}
 	}
 
 	public int maxStrokeWidth() {
